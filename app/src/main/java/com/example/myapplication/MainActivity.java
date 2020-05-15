fe46adb891b3060ea3b839983a520ea2e30c80a0
seckin hezarfen <seckin.atilmis@bilgiedu.net>
2020-05-15 22:37:10 +0300

    hava durumu apisiyle veri aldım onları işlemek kaldı haritada sağ alttaki tuşla diğer aktiviteye geçiyor zaten çalışmıyordu onları hallederiz eksik bulduğunuz  yerleri yazın yine

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -65,7 +68,28 @@ public class MainActivity extends AppCompatActivity {
         //lintteki bilgileri stringe atıp gösterdik şimdi JSON'u prse yapıp ihtiyacımız olanları çekebiliriz.
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            System.out.println("alınan data: "+ s);
+            //System.out.println("alınan data: "+ s);
+            //JSON object{} ve JSON array[] kullanacagız
+            try {
+               //Json objesiyle apideki veriyi çekmece,çalışıyor.
+                /*JSONObject jsonObject=new JSONObject(s);
+                String deneme=jsonObject.getString("id");
+                System.out.println("deneme: "+ deneme);*/
+                JSONObject jsonObject=new JSONObject(s);
+                String deneme=jsonObject.getString("coord");
+                System.out.println("coordinates:"+deneme);
+                JSONObject jsonObject1=new JSONObject(deneme);
+                String lon=jsonObject1.getString("lon");
+                System.out.println("coordinates:"+lon);
+                String lat=jsonObject1.getString("lat");
+                System.out.println("coordinates:"+lat);
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
